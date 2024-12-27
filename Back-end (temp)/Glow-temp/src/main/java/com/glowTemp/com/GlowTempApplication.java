6791a4621e55a4382947e2b6c95aaa08dc407fa9
Rishav Raj <rishav3330@gmail.com>
2024-12-27 19:02:57 +0530

    project set-up

---
+++ Back-end (temp)/Glow-temp/src/main/java/com/glowTemp/com/GlowTempApplication.java
@@ -0,0 +1,13 @@
+package com.glowTemp.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowTempApplication.class, args);
+	}
+
+}
