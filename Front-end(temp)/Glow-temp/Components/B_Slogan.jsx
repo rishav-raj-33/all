4b615fbd3241a7f87a927cde39a804f582fa63a9
Rishav Raj <rishav3330@gmail.com>
2024-12-29 07:50:08 +0530

    UI design and Front-End work start

---
+++ Front-end(temp)/Glow-temp/Components/B_Slogan.jsx
@@ -0,0 +1,34 @@
+function B_Slogan() {
+
+
+    return (
+
+        <div className="container-fluid" id="slogan">
+
+            <h1 className="text-center" id="slogan-h1">Exploring the intersection of</h1> <h1 className="text-center" id="tech">Technologies </h1>
+            <h5 className="text-center" id="slogan-h5">Push the limits of innovation by combining popular technologies </h5>
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+    )
+
+
+
+
+
+
+
+}
+
+
+
+export default B_Slogan;
--- Front-end(temp)/Glow-temp/src/App.jsx
+++ Front-end(temp)/Glow-temp/src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import useLocalStorage from 'use-local-storage'
 import './App.css'
+import B_Slogan from '../Components/B_Slogan';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkTheme, setDarkTheme] = useLocalStorage("darkTheme", false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+
+
+      <div data-theme={darkTheme ? "dark" : "light"}>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" id="header">
+          <div className="container-fluid">
+            <div id="div-logo">
+            <i className="bi bi-lightbulb-fill" id='logo'></i>
+              <h1 id="invoice">Glow</h1>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span><i className="bi bi-blockquote-right"></i></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a  className="nav-link active" aria-current="page">Home</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page">Filters</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page">Built With</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page">Get in Touch</a>
+                </li>
+              </ul>
+
+              {/* Theme */}
+
+              <div id="theme-box">
+                <input type="checkbox" className="checkbox" id="checkbox" onChange={() => {
+                  setDarkTheme(!darkTheme);
+                }} checked={darkTheme} />
+                <label htmlFor="checkbox" className="checkbox-label">
+                  <i className="bi bi-moon-stars-fill" id="fa-moon"></i>
+                  <i className="bi bi-brightness-high-fill" id="fa-sun"></i>
+                  <span className="ball"></span>
+                </label>
+              </div>
+            </div>
+
+          </div>
+        </nav>
+      <hr className="line"></hr>
+
+
+      <B_Slogan />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      
+
   )
 }
 
