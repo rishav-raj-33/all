d1a6aaf3a845f84a9bd5681f96ca06ac8fade21b
Rishav Raj <rishav3330@gmail.com>
2024-12-30 10:40:59 +0530

    tech infov9

--- Front-end(temp)/Glow-temp/Components/B_Slogan.jsx
+++ Front-end(temp)/Glow-temp/Components/B_Slogan.jsx
@@ -1,3 +1,5 @@
+import canny from '/src/assets/canny.jpg';
+
 function B_Slogan() {
 
 
@@ -7,13 +9,19 @@ function B_Slogan() {
 
             <h1 className="text-center" id="slogan-h1">Exploring the intersection of</h1> <h1 className="text-center" id="tech">Technologies </h1>
             <h5 className="text-center" id="slogan-h5">Push the limits of innovation by combining popular technologies </h5>
+           
+           <div className="row justify-content-evenly" id="filter-description-div">
+            <div className="col-6">
+                <img width={600} className='image-fluid' src={canny}></img>
+            </div>
+                <div className="col-6" id="photo-content">
+                    
 
+                </div>
 
+           </div>
 
-
-
-
-
+ 
 
         </div>
 
